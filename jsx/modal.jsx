eb7c80312dc0e38b5b3a9be9eb3850a7ac17ab97
jaikathuria <jaikathuria@live.com>
2017-03-07 01:10:15 +0530

    Added Modal.jsx

---
+++ jsx/modal.jsx
@@ -0,0 +1,16 @@
+var index = 0;
+
+var TODO = [
+    {
+        title: " Complete TODO App ",
+        date: new Date(),
+        id: index,
+        completed: false,
+    },
+    {
+        title: " Pata Nahi  ",
+        date: new Date(),
+        id: index + 1,
+        completed: true,
+    },
+];
