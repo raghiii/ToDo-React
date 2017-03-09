26e84c6f57593281d85d4f36b9f950a9e7cf2e43
jaikathuria <jaikathuria@live.com>
2017-03-09 08:22:16 +0530

    UPDATE: Js file artcitecure

---
+++ data/data.js
@@ -0,0 +1,14 @@
+var TODO = [
+    {
+        title: "Complete TODO App",
+        date: new Date(),
+        id: 0,
+        completed: false,
+    },
+    {
+        title: "13 Resons Why - e10",
+        date: new Date(),
+        id: 1,
+        completed: false,
+    },
+];
