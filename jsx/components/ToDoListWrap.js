4262a4f7ba67f8e5d71b0758b3d81883dc3fab99
jaikathuria <jaikathuria@live.com>
2017-03-13 02:00:54 +0530

    ToDoListWrap: Component converted to es6 react component

--- jsx/components/ToDoListWrap.js
+++ jsx/components/ToDoListWrap.js
@@ -1,43 +1,40 @@
 //Importing Libraries
-import React from 'react';
-
+import React, {Components, PropTypes} from 'react';
+//Importing UI Components
 import EmptyToDo from './EmptyToDo';
 import ToDoList from './ToDoList';
 
-var ToDoListWrap = React.createClass({
-    propTypes: {
-        todoArr: React.PropTypes.array.isRequired,
-        changeStatus: React.PropTypes.func.isRequired,
-        remove: React.PropTypes.func.isRequired,
-    },
+export default class ToDoListWrap extends Components {
 
-    changeStatus: function(index,bool){
-        this.props.changeStatus(index,bool);
-    },
+  changeStatus(index, bool) {
+    this.props.changeStatus(index, bool);
+  },
 
-    remove: function(index){
-      this.props.remove(index);
-    },
+  remove(index) {
+    this.props.remove(index);
+  },
 
-    render: function() {
-      return (
-        <div className="row">
-          <div className="col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-             {
-                this.props.todoArr.length > 0 ?
-                    <ToDoList
-                        todos={this.props.todoArr}
-                        changeStatus={ function(index,bool){ this.changeStatus(index,bool)}.bind(this) }
-                        remove={ function(index){ this.remove(index) }.bind(this) }
-                    />
-                :
-                    <EmptyToDo />
-             }
-          </div>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+          {
+          this.props.todoArr.length > 0
+            ? <ToDoList todos={this.props.todoArr} changeStatus={function(index, bool) {
+                this.changeStatus(index, bool)
+              }.bind(this)} remove={function(index) {
+                this.remove(index)
+              }.bind(this)}/>
+            : <EmptyToDo/>
+          }
         </div>
-      )
-    },
-});
+      </div>
+    )
+  }
+}
 
-
-export default ToDoListWrap;
+ToDoListWrap.propTypes = {
+  todoArr: React.PropTypes.array.isRequired,
+  changeStatus: React.PropTypes.func.isRequired,
+  remove: React.PropTypes.func.isRequired
+};
