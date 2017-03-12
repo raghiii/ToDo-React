e1a346e640ad5d7a0c232a234b86569032c3ef51
jaikathuria <jaikathuria@live.com>
2017-03-13 01:00:54 +0530

    ToDoItem: Component converted to es6 react component

--- jsx/components/ToDoItem.js
+++ jsx/components/ToDoItem.js
@@ -1,53 +1,60 @@
 //Importing Libraries
-import React from 'react';
+import React, {Components, PropTypes} from 'react';
 
-var ToDoItem = React.createClass({
+export default class ToDoItem extends Components {
 
-    propTypes: {
-        title: React.PropTypes.string.isRequired,
-        completed: React.PropTypes.bool.isRequired,
-        status: React.PropTypes.func.isRequired,
-        remove: React.PropTypes.func.isRequired,
-    },
+  done() {
+    this.props.status(true);
+  }
 
-    done: function(){
-        this.props.status(true);
-    },
+  undone() {
+    this.props.status(false);
+  }
 
-    undone: function(){
-        this.props.status(false);
-    },
+  remove() {
+    this.props.remove();
+  }
 
-    remove: function(){
-        this.props.remove();
-    },
+  render() {
+    return (
+      <li className="todo-item">
+        {
+          this.props.completed
+          ? (
+            <div>
+              <div className="todo-title done">
+                {this.props.title}
+              </div>
+              <div className="todo-button pull-right hover-light" onClick={this.remove}>
+                <i className="fa fa-trash-o"></i>
+              </div>
+              <div className="todo-button pull-right hover-light" onClick={this.undone}>
+                <i className="fa fa-undo"></i>
+              </div>
+            </div>
+          )
+          : (
+            <div>
+              <div className="todo-title">
+                {this.props.title}
+              </div>
+              <div className="todo-button pull-right hover-light" onClick={this.remove}>
+                <i className="fa fa-trash-o"></i>
+              </div>
+              <div className="todo-button pull-right hover-light" onClick={this.done}>
+                <i className="fa fa-check"></i>
+              </div>
+            </div>
+          )
+        }
+      </li>
+    )
+  }
+}
 
-    render: function() {
-        return (
-            <li className="todo-item">
-                {
-                 this.props.completed ?
-                 (
-                     <div>
-                        <div className="todo-title done"> {this.props.title} </div>
-                        <div className="todo-button pull-right hover-light" onClick={this.remove}> <i className="fa fa-trash-o"> </i> </div>
-                        <div className="todo-button pull-right hover-light" onClick={this.undone}> <i className="fa fa-undo"> </i> </div>
-                     </div>
-                 )
-                 :
-                 (
-                     <div>
-                        <div className="todo-title"> {this.props.title} </div>
-                        <div className="todo-button pull-right hover-light" onClick={this.remove}> <i className="fa fa-trash-o"> </i> </div>
-                        <div className="todo-button pull-right hover-light" onClick={this.done}> <i className="fa fa-check"> </i> </div>
-                     </div>
-                 )
-                }
-            </li>
-        )
-    },
-});
-
-
-
-export default ToDoItem;
+ToDoItempropTypes = {
+  title: React.PropTypes.string.isRequired,
+  completed: React.PropTypes.bool.isRequired,
+  status: React.PropTypes.func.isRequired,
+  remove: React.PropTypes.func.isRequired
+};
